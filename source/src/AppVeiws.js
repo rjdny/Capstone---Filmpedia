f705e4bbb3eff9bd1bf18ad0b849864f8d07d31f
Ryan Downey <105568842+rjdny@users.noreply.github.com>
2022-08-01 13:36:07 -0500

    MVP(Cleaned)

---
+++ source/src/AppVeiws.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Route,Routes } from "react-router";
+import Navigation from "./Navigation/Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import { Database } from "./Database/database";
+import { LoginForm } from "./LoginForm/LoginForm";
+import {FourOFour} from "./404/FourOFour";
+import { Movies } from "./Movies/Movies";
+import MoviePage from "./MoviePage/MoviePage";
+import { Favorites } from "./Movies/Favorites";
+
+export default function AppVeiws() {
+
+  return (
+    Database.GetIsLoggedIn() ? 
+    <>
+      <Routes>
+        <Route path="/movies" element={<><Navigation/><Movies/></>}/>
+        <Route path="/movies/:movieId/" element={<><Navigation/> <MoviePage/></>}/>
+        <Route path="/favorites" element={<><Navigation/><Favorites/></>}/>
+        <Route path="*" element={<FourOFour/>} />
+      </Routes>
+    </>:<>
+    <LoginForm/>
+    </>
+  );
+}
---
+++ source/src/Movies/Favorites.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Database } from '../Database/database';
+import { useState,useEffect } from 'react';
+import './Movies.css';
+import MovieCard from '../MovieCard/MovieCard';
+import { useNavigate } from 'react-router';
+
+
+
+
+export function Favorites(props){
+    const [currHtml,setcurrHtml] = useState([])
+    const nav = useNavigate()
+
+    var likesArray = []
+    fetch("http://localhost:8088/likes")
+    .then((response) => response.json())
+    .then((obj) => obj).then((obj) => {
+        likesArray = obj;
+    });
+
+    useEffect(() => {
+        fetch("http://localhost:8088/movies")
+        .then((response) => response.json())
+        .then((obj) => obj).then((obj) => {
+            var movies = obj;
+            var currentUser = Database.Get_CurrentUser();
+            var mylikes = likesArray.filter((like) => like.userId === currentUser.id)
+
+            var moviesToRender = []
+            mylikes.forEach(like => {
+                moviesToRender.push(movies.find((movie) => movie.id === like.movieId))
+            });
+
+            setcurrHtml(<>
+            <div onClick={(e) => {onClickMovie(e,movies,nav)}} className="holder">{moviesToRender.map((movie,index) => (<MovieCard index={index} movie={movie}></MovieCard>))}</div>
+            </>)
+        })
+
+    },[])
+    return currHtml;
+};
+
+
+const onClickMovie = (event,allmovies,nav) => {
+    var movieId = event.target.id;
+    nav(`/movies/${movieId}`)
+}
